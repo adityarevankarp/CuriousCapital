36e08c258b535760aacffc02f31c41088f9da293
AchyuthPRao <achyuthprao123@gmail.com>
2024-03-09 16:29:24 +0530

    Landing Page added

---
+++ frontend/src/Components/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import bg from "../assets/home";
+
+function Landing() {
+  return (
+    <div className="bg-cover bg-center h-screen bg-img">
+      <h1>Hi nigga</h1>
+    </div>
+  );
+}
+
+export default Landing;
