e847c143751586a09300da5828b4ac9454dbe03c
AchyuthPRao <achyuthprao123@gmail.com>
2024-03-10 20:50:17 +0530

    Navbar Done

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -8,18 +8,18 @@ const Navbar = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
   return (
-    <nav style={{ backgroundColor: "rgba(0,0,0,0.1)" }} className="navi">
-      <div className="max-w-screen-xl mx-auto p-4">
+    <nav style={{ backgroundColor: "rgba(0,0,0,0.3)" }} className="navi">
+      <div className="max-w-screen-xl lg:mx-auto p-4 custom-nav">
         <button
           onClick={toggleMobileMenu}
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-transparent focus:outline-none focus:ring-2 focus:ring-transparent dark:text-gray-400 dark:hover:bg-transparent dark:focus:ring-transparent"
+          className="inline-flex items-center p-2 w-30 h-30 justify-center text-sm rounded-lg md:hidden hover:bg-transparent focus:outline-none focus:ring-2 focus:ring-transparent dark:text-gray-400 dark:hover:bg-transparent dark:focus:ring-transparent"
           aria-controls="navbar-default"
           aria-expanded={isMobileMenuOpen}
         >
-          <span className="sr-only">Open main menu</span>
-          <svg
+          {/* <span className="sr-only">Open main menu</span> */}
+          {/* <svg
             className="md:w-5 md:h-5 "
             aria-hidden="true"
             fill="none"
@@ -32,19 +32,20 @@ const Navbar = () => {
               strokeWidth="2"
               d="M1 1h15M1 7h15M1 13h15"
             />
-          </svg>
+          </svg> */}
+          <img src={logo} className="h-8 w-20" alt="" />
         </button>
         <div
           className={`w-full md:block md:w-auto ${
             isMobileMenuOpen ? "block" : "hidden"
-          }`}
+          } sm:flex transition ease-in-out duration-700 transform`}
           id="navbar-default"
         >
-          <ul className="navbar-custom font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent dark:bg-transparent">
+          <ul className="navbar-custom lg:gap-24 md:gap-24 sm:gap-20 sm:justify-start text-white text-xl font-bold flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent ">
             <li>
               <a
                 href="/"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-white-500"
+                className="block  py-2 px-3 rounded md:bg-transparent md:dark:hover:text-pink-200   md:p-0 dark:text-white md:dark:text-white-500"
                 aria-current="page"
               >
                 Home
@@ -53,7 +54,7 @@ const Navbar = () => {
             <li>
               <a
                 href="/about"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-transparent"
+                className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-pink-200 dark:hover:bg-transparent"
               >
                 About
               </a>
@@ -63,22 +64,22 @@ const Navbar = () => {
                 href="/"
                 className="flex items-center space-x-3 rtl:space-x-reverse "
               >
-                <img src={logo} className="h-7 logo" alt="" />
+                <img src={logo} className="h-8 logo" alt="" />
               </a>
             </li>
 
             <li>
               <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-transparent"
+                href="/portfolio"
+                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-pink-200 dark:hover:bg-transparent"
               >
                 Portfolio
               </a>
             </li>
             <li>
               <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-transparent"
+                href="/blogs"
+                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-pink-200 dark:hover:bg-transparent"
               >
                 Blogs
               </a>
--- frontend/src/Pages/Portfolio.jsx
+++ frontend/src/Pages/Portfolio.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Hero from "../Components/Hero";
 
 function Portfolio() {
   return (
     <div>
-      <div>Portfolio</div>
+      <div>
+        <Hero />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
