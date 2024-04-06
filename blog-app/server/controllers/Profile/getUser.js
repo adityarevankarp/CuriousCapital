abff782c86cb9aaf45f8a029fe025afcb3711f32
ManishMadan2882 <manishmadan321@gmail.com>
2024-04-06 12:40:49 +0530

    sort blogs, latest first

--- blog-app/server/controllers/Profile/getUser.js
+++ blog-app/server/controllers/Profile/getUser.js
@@ -2,7 +2,7 @@ const {user,blogs} = require('../../database/schema');
 const getUserById = async (req,res)=>{
   try{
    let searchedUser = await user.findOne({username:req.params.username});
-   searchedUser.blogs = await blogs.find({account : searchedUser._id});
+   searchedUser.blogs = await blogs.find({account : searchedUser._id}).sort({createdAt : -1});
    if(searchedUser == null) 
    return res.status(404).json({msg:'user not found'})
 
